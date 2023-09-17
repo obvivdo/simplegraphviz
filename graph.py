55587726ab93aad388be103ce367ee120fb9e14c
obvivdo <calm@DESKTOP-TGHT6RN.localdomain>
2023-09-17 19:17:38 +1000

    added basic vr patern

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 import graphviz
+# pip3 install graphviz 
 
-
-FILE = 'bitsadmin.csv'
+FILE = 'basic_vr_patterns.csv'
 FILENAME = FILE.split('.')[0]
 OUTFILE = FILENAME + '_output' # no .pdf
 
@@ -24,26 +24,40 @@ def get_input(FILE):
 def make_graph(nodes,rows):
     # data is a list ?
     f = graphviz.Digraph('finite_state_machine', filename='fsm.gv')
-    f.attr(rankdir='LR', size='8,5')
+    f.attr(rankdir='LR', size='12,9')
 
     # define Nodes
-    f.attr('node', shape='doublecircle')
+    f.attr('node', shape='egg')
     for n in nodes:
         f.node(n)
+        
 
-
+    table_str =  '''<
+    <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
+    '''
     # Define edges 
-    f.attr('node', shape='circle')
+    
     for i,e in enumerate(rows):
         label = f'{str(i)}. {e[2]}'
         f.edge(e[0], e[1], label)
 
+        table_str += f'''<TR>
+        <TD COLSPAN="3">{label}</TD>
+        </TR>
+        '''
+    table_str += '''</TABLE>>'''
+
+    if table:
+        f.attr('node', shape='plaintext')
+        f.node('struct1', table_str)
 
     # make it so 
     f.render(f'./outputs/{OUTFILE}')  
 
 
 if __name__ == '__main__':
-     nodes, rows = get_input(FILE)
-     make_graph(nodes,rows)
+    table = False
+    nodes, rows = get_input(FILE)
+    make_graph(nodes,rows)
+    #make_table()
 
